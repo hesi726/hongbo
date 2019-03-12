299b1b2d46c41b2d6c3e688fc07f4d0bbe1f92e8
dear.daiwei <dear.daiwei@hotmail.com>
2019-03-12 22:01:18 +0800

    添加 MovieUtil 的UnitTest

---
+++ hongboExtension.test/DrawingExtension/MovieUtil.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace hongbao.DrawingExtension
+{
+    /// <summary>
+    /// 视频的工具类； 
+    /// </summary>
+    [TestClass]
+    public class MovieUtilTest
+    {
+        /// <summary>
+        /// 获取视频文件长度； 
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public void GetMediaTimeLenSecond()
+        {
+
+            string filepath = @"";
+            var result = MovieUtil.GetMediaTimeLenSecond(filepath);
+            Assert.IsTrue(result > 0);
+        }
+    }
+}
--- hongboExtension/DrawingExtension/MovieUtil.cs
+++ hongboExtension/DrawingExtension/MovieUtil.cs
@@ -16,13 +16,14 @@ namespace hongbao.DrawingExtension
         /// <summary>
         /// 获取视频文件长度； 
         /// </summary>
-        /// <param name="path"></param>
+        /// <param name="pathToVideoFile"></param>
         /// <returns></returns>
         public static int GetMediaTimeLenSecond(string pathToVideoFile)
         {
             try
             {
                 var ffProbe = new NReco.VideoInfo.FFProbe();
+               // ffProbe.FFProbeExeName = @"E:\ffmpeg-20190310-5ab44ff-win64-static\bin\ffprobe.exe";
                 var videoInfo = ffProbe.GetMediaInfo(pathToVideoFile);
                 return (int) videoInfo.Duration.TotalSeconds;
             }
